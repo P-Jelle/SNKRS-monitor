c0c26dcff94cff09a81aa1783057abea7f86daf8
P-Jelle <150236863+P-Jelle@users.noreply.github.com>
2025-01-18 02:07:15 +0100

    Update monitor.js

--- scripts/monitor.js
+++ scripts/monitor.js
@@ -41,7 +41,7 @@ fetch(proxyUrl + apiFilter, {
                 const color = product.publishedContent?.properties?.coverCard?.properties?.title || "";
                 const exclusiveAccess = releaseDetail.merchProduct?.exclusiveAccess ? "Yes" : "No";
                 const launchType = launchConverter[releaseDetail.launchView?.method] || "";
-                const image = product.publishedContent?.nodes[0]?.nodes[0]?.properties?.squarish?.url || "";
+                const image = product.publishedContent?.nodes?.[0]?.nodes?.[0]?.properties?.squarish?.url || "";
                 const date = releaseDetail.launchView?.startEntryDate || "";
                 const parsedDate = date ? new Date(date) : null;
                 const launchDate = parsedDate ? parsedDate.toLocaleDateString() : "";
